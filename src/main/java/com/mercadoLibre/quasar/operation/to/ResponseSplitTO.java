aa2f6c493f2462399c43b0c16030162367e75ed0
AndresBKY75 <stiedan@gmail.com>
2021-06-09 21:55:18 -0500

    Ajuste swagger

--- src/main/java/com/mercadoLibre/quasar/operation/to/ResponseSplitTO.java
+++ src/main/java/com/mercadoLibre/quasar/operation/to/ResponseSplitTO.java
@@ -2,11 +2,17 @@ package com.mercadoLibre.quasar.operation.to;
 
 import java.io.Serializable;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "Datos de la respuesta para una nave especifica")
 public class ResponseSplitTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@ApiModelProperty(value = "Distancia de una nave")
 	private Double distance;
+	@ApiModelProperty(value = "Mensaje de una nave")
 	private String[] message;
 	
 	public Double getDistance() {
--- src/main/java/com/mercadoLibre/quasar/operation/to/requestTO.java
+++ src/main/java/com/mercadoLibre/quasar/operation/to/requestTO.java
@@ -3,10 +3,15 @@ package com.mercadoLibre.quasar.operation.to;
 import java.io.Serializable;
 import java.util.List;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "Datos de la petición")
 public class requestTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@ApiModelProperty(value = "Lista con datos de las naves")
 	private List<quasarTO> satellites;
 
 	public List<quasarTO> getSatellites() {
