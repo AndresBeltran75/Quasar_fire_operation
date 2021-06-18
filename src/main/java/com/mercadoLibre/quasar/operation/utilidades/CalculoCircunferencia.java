c2a8099244bc4fd0b6c538f817da3094861ed955
AndresBKY75 <stiedan@gmail.com>
2021-06-18 00:21:35 -0500

    ajustes generales

--- src/main/java/com/mercadoLibre/quasar/operation/utilidades/CalculoCircunferencia.java
+++ src/main/java/com/mercadoLibre/quasar/operation/utilidades/CalculoCircunferencia.java
@@ -9,19 +9,24 @@ import com.mercadoLibre.quasar.operation.to.PuntoTO;
 public class CalculoCircunferencia {
 	
 	public CircunferenciaTO calcularCentro(PuntoTO x1, PuntoTO x2, PuntoTO x3) {
-        CircunferenciaTO circunferencia = new CircunferenciaTO();
+        
+		CircunferenciaTO circunferencia = new CircunferenciaTO();
         double a, b, c, d, e, f, h, k, r;
-        a = x1.getPunto().getX();
-        b = x1.getPunto().getY();
-        c = x2.getPunto().getX();
-        d = x2.getPunto().getY();
-        e = x3.getPunto().getX();
-        f = x3.getPunto().getY();
-        h = (0.5) * ((Math.pow(a, 2) + Math.pow(b, 2)) * (f - d) + (Math.pow(c, 2) + Math.pow(d, 2)) * (b - f) + (Math.pow(e, 2) + Math.pow(f, 2)) * (d - b)) / (a * (f - d) + c * (b - f) + e * (d - b));
-        k = (0.5) * ((Math.pow(a, 2) + Math.pow(b, 2)) * (e - c) + (Math.pow(c, 2) + Math.pow(d, 2)) * (a - e) + (Math.pow(e, 2) + Math.pow(f, 2)) * (c - a)) / (b * (e - c) + d * (a - e) + f * (c - a));
-        r = Math.sqrt(Math.pow((h - a), 2) + Math.pow((k - b), 2));
-        circunferencia.setRadio(r);
-        circunferencia.setCentro(new PuntoTO((int) h, (int) k));
+        
+        if( x1 != null && x2 != null && x3 != null ) {
+	        a = x1.getPunto().getX();
+	        b = x1.getPunto().getY();
+	        c = x2.getPunto().getX();
+	        d = x2.getPunto().getY();
+	        e = x3.getPunto().getX();
+	        f = x3.getPunto().getY();
+	        h = (0.5) * ((Math.pow(a, 2) + Math.pow(b, 2)) * (f - d) + (Math.pow(c, 2) + Math.pow(d, 2)) * (b - f) + (Math.pow(e, 2) + Math.pow(f, 2)) * (d - b)) / (a * (f - d) + c * (b - f) + e * (d - b));
+	        k = (0.5) * ((Math.pow(a, 2) + Math.pow(b, 2)) * (e - c) + (Math.pow(c, 2) + Math.pow(d, 2)) * (a - e) + (Math.pow(e, 2) + Math.pow(f, 2)) * (c - a)) / (b * (e - c) + d * (a - e) + f * (c - a));
+	        r = Math.sqrt(Math.pow((h - a), 2) + Math.pow((k - b), 2));
+	        circunferencia.setRadio(r);
+	        circunferencia.setCentro(new PuntoTO((int) h, (int) k));	
+        }
+        
         return circunferencia;
     }
 
